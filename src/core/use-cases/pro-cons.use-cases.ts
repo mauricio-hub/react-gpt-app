38c307096c39968fd0759f5088f478484fe80dfa
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-01-28 23:27:37 -0500

    agregando vista pros-cons

---
+++ src/core/use-cases/pro-cons.use-cases.ts
@@ -0,0 +1,32 @@
+import type { ProsConsResponse } from "../../interfaces";
+
+export const prosConsUseCases = async (prompt: string) => {
+  try {
+    const resp = await fetch(
+      `${import.meta.env.VITE_GPT_API}/pros-cons-discusser`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }),
+      }
+    );
+
+    if (!resp.ok) {
+      throw new Error("No se pudo realizar la comparacion");
+    }
+
+    const data = (await resp.json()) as ProsConsResponse
+
+    return {
+      ok: true,
+      ...data,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      content: 'No se pudo realizar la comparacion'
+    };
+  }
+};
--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,6 +1,62 @@
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TypingLoader,
+  TextMessageBox,
+} from "../../components";
+import { prosConsUseCases } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
 
 export const ProsConsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    //llama al caso de uso
+    const response = await prosConsUseCases(text);
+    const { ok, content } = response;
+    if (!ok) return;
+
+    setMessages((prev) => [...prev, { text: content, isGpt: true }]);
+
+    setIsLoading(false);
+  };
+
   return (
-    <div>ProsConsPage</div>
-  )
-}
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="Escribe lo que quiereas cmparar" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe tu mensaje aquí"
+        disabledCorretions
+      />
+    </div>
+  );
+};
